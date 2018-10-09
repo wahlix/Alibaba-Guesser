08f648c7cb1a364e32d86e07090c532b0655f667
Rickard Wahlander <rickard@wahlix.nu>
2018-10-09 11:20:49 +0200

    v.0.0.4
    Initial message inserted

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 /*Alibaba Guesser by Rickard Wahlander - NSArg18
-v 0.0.3
+v 0.0.4
 Todo:
 */
 #include <iostream>
 #include <ctime>
 using namespace std;
 
+int correctNumber, playerGuess, randNumber, wrongNumber;
+
+int main()
+{
+	cout << "Welcome to Alibaba Guesser!" << endl;
+	cout << "I'm thinking of a number between 1 and 50" << endl;
+	cout << "Your task is to guess the number I'm thinking of." << endl;
+	cout << "Your guess: ";
+	cin >> playerGuess;
+
+		playerGuess()
+		srand() 
+	return 0;
+}
