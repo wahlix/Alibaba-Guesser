1342dc26c67cdc3c75985145172d1d474ce642f6
Rickard Wahlander <rickard@wahlix.nu>
2018-10-17 09:05:33 +0200

    v.0.0.5
    More code - ish

--- main.cpp
+++ main.cpp
@@ -6,17 +6,38 @@ Todo:
 #include <ctime>
 using namespace std;
 
-int correctNumber, playerGuess, randNumber, wrongNumber;
+int correctNumber, playerGuess, randNumber, wrongNumber, guessedNumbers[5] = { }, points;
+bool guessLoop
 
 int main()
 {
+	randNumber = 5;
+	guessLoop = true;
 	cout << "Welcome to Alibaba Guesser!" << endl;
 	cout << "I'm thinking of a number between 1 and 50" << endl;
 	cout << "Your task is to guess the number I'm thinking of." << endl;
-	cout << "Your guess: ";
-	cin >> playerGuess;
+	while (guessLoop == true){
+		cout << "Your guess: ";
+		cin >> playerGuess
+		if (playerGuess > randNumber);
+		{
+			cout << "Sorry, you guessed too high, try again!";
+			guessLoop == true;
+		}
+		else if (playerGuess < randNumber);
+		{
+			cout << "Sorry, you guessed too low, try again";
+			guessLoop == true;
+		}
+		else if (playerGuess = randNumber);
+		{
+			cout << "Congratulations, you guessed right!";
+			guessLoop = false;
 
-		playerGuess()
-		srand() 
+		}	
+	}
+	
+		
+		
 	return 0;
 }
